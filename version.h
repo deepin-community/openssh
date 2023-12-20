20d305949a43a619d27468c1a1d8ed3a6f870700
Tianyu Chen <sweetyfish@deepin.org>
2023-12-20 13:51:59 +0800

    feat: update openssh to 1:9.6p1-1

--- version.h
+++ version.h
@@ -1,6 +1,6 @@
-/* $OpenBSD: version.h,v 1.94 2022/04/04 22:45:25 djm Exp $ */
+/* $OpenBSD: version.h,v 1.100 2023/12/18 14:48:44 djm Exp $ */
 
-#define SSH_VERSION	"OpenSSH_9.0"
+#define SSH_VERSION	"OpenSSH_9.6"
 
 #define SSH_PORTABLE	"p1"
 #define SSH_RELEASE	SSH_VERSION SSH_PORTABLE
